ad8a8600464658a12e958d8feacd4a46e5de3ac4
javaria-gul <javariagul73@gmail.com>
2025-05-23 12:04:16 +0500

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SE_TRENDZZ.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SE_TRENDZZ.MvcApplication" Language="C#" %>
